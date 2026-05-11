59916962cb716008534bde76c6c4b8413314ee49
Wizc0der <wizcoder0@gmail.com>
2026-05-11 10:56:04 +0300

    Delete Day 18 directory

--- Day 18/Task 1/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Task_1.Models
-{
-    public class User
-    {
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; } = "Guest";
-    }
-}
